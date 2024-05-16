265914e014386275040908cf904d0f1bebab25d2
Ngo Minh Thien <thienngo.081003@gmail.com>
2024-05-16 22:54:57 +0700

    add server

--- client/NoteProject/src/utils/constantRequest.js
+++ client/NoteProject/src/utils/constantRequest.js
@@ -1,2 +1,3 @@
-export const GRAPHQL_URL = "http://localhost:4000";
-export const GRAPHQL_WEBSOCKER_URL_ENPOINT = "ws://localhost:4000/graphql";
+export const GRAPHQL_URL = "https://noteproject.onrender.com";
+export const GRAPHQL_WEBSOCKER_URL_ENPOINT =
+  "ws://https://noteproject.onrender.com/graphql";
