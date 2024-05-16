8ff8028d1786ddab4365055f3efa3c8232935c7b
Ngo Minh Thien <thienngo.081003@gmail.com>
2024-05-16 23:04:32 +0700

    add wss

--- client/NoteProject/src/utils/constantRequest.js
+++ client/NoteProject/src/utils/constantRequest.js
@@ -1,3 +1,3 @@
 export const GRAPHQL_URL = "https://noteproject.onrender.com";
 export const GRAPHQL_WEBSOCKER_URL_ENPOINT =
-  "ws://https://noteproject.onrender.com/graphql";
+  "wss://https://noteproject.onrender.com/graphql";
