b2156ab9ab70eb071e967d0cff6c627f597a8bbe
Ngo Minh Thien <thienngo.081003@gmail.com>
2024-05-16 23:09:38 +0700

    Update url ws

--- client/NoteProject/src/utils/constantRequest.js
+++ client/NoteProject/src/utils/constantRequest.js
@@ -1,3 +1,3 @@
 export const GRAPHQL_URL = "https://noteproject.onrender.com";
 export const GRAPHQL_WEBSOCKER_URL_ENPOINT =
-  "wss://https://noteproject.onrender.com/graphql";
+  "wss://noteproject.onrender.com/graphql";
